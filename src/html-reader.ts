2491b7a4a80a1ee77cff468a314ac292a0ee1374
flektor <gdrosin@gmail.com>
2025-03-22 21:44:39 +0100

    refactor examples & clean-up dead code

--- src/html-reader.ts
+++
@@ -1,9 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-export async function htmlReader(url: string) {
-  const dom = await JSDOM.fromFile(url, {
-    includeNodeLocations: true,
-  });
-
-  return new JSDOM(dom.serialize());
-}
