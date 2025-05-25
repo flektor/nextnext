c16bcc3b7beeeaa00dd32e242d297980665d74e4
flektor <gdrosin@gmail.com>
2025-05-25 12:28:52 +0200

    added src folder in the examples

--- examples/app/src/App.jsx
+++ examples/app/src/App.jsx
@@ -3,3 +3,5 @@ import { Home } from "./Home.jsx"
 export function App() {
   return <Home isTrue={false} />
 }
+
+
--- examples/app/src/components/Button.jsx
+++ examples/app/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({  color, onClick }) {
   const label = "Click me!"
-  const className = color === "red" ? "bg-red-500" : "bg-blue-500";
+  const className = color === "red" ? "text-red" : "text-red";
   const button = <button class={className} onClick={onClick}>
     {label}
     </button>
